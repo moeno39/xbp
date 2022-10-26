e8682b82c1c41d332f49a65407ce657e67f3f716
moeno39 <r202202197cv@jindai.jp>
2022-10-27 03:20:10 +0900

    20221027

--- de12/python/1.py
+++ de12/python/1.py
@@ -2,21 +2,32 @@ print("あなたは授業で居眠りをしていて目が覚めると教室に�
 print("...? 自分以外に誰かいるようだ。")
 name=input("A君 あなたの名前は？")
 
+print("A君",name,"さんだね！")
 print("A君 閉じ込められてるみたいだけど、どうする？？")
 print("１周りを見渡す")
 print("２諦めて寝る")
 print("３目の前にあるお菓子を食べる")
 
-selecta=int(input("1~3の中から選んでね！"))
+for selecta in range(2 and 3):
+    selecta=int(input("1~3の中から選んでね!"))
 
-if selecta==1:
-    print("教室の中に道用先生のパソコンを見つけた！")
+    if selecta==1: 
+        print("教室の中に道用先生のパソコンを見つけた！")
+        break
+        
 
-elif selecta==2:
-    print("気が付いたら朝になっていた！")
+    elif selecta==2:
+        print("気が付いたら朝になっていた！")
+        print("もう一度選んでみよう！")
+        continue
+        
+       
+    elif selecta==3:
+        print("お菓子の食べ過ぎで動けなくなってしまった！")
+        print("もう一度選んでみよう！")
+        continue
 
-elif selecta==3:
-    print("お菓子の食べ過ぎで動けなくなってしまった！")
+    
 
 
 print("パソコンのパスワードを入れてみよう！")
@@ -24,16 +35,23 @@ print("１道用先生の誕生日")
 print("２道用先生の身長")
 print("３道用先生の体重")
 
-selectb=int(input("1~3の中から選んでね！"))
+for selectb in range(1 and 2):
+    selectb=int(input("1~3の中から選んでね!"))
 
-if selectb==3:
-    print("教室のドアからガチャという音がした！")
+    if selectb==3:
+        print("教室のドアからガチャという音がした！")
+        break
 
-elif selectb==1:
-    print("パスワードが違うようだ")
+    elif selectb==1:
+        print("パスワードが違うようだ")
+        print("もう一度選んでみよう！")
+        continue
 
-elif selectb==2:
-    print("パスワードが違うようだ")
+    elif selectb==2:
+        print("パスワードが違うようだ")
+        print("もう一度選んでみよう！")
+        continue
+
 
 print("教室から出ると、下の階が火事になっていた！")
 
@@ -42,29 +60,39 @@ print("どうしよう！")
 print("１上の階に行く")
 print("２下の階に行く")
 
-selecta=int(input("1~2の中から選んでね！"))
+for selectc in range(2):
+    selectc=int(input("1~2の中から選んでね!"))
 
-if selecta==1:
-    print("屋上に出ることができた！")
+    if selectc==1:
+        print("屋上に出ることができた！")
+        break
 
-elif selecta==2:
-    print("火に囲まれて逃げられなくなった！")
+    elif selectc==2:
+        print("火に囲まれて逃げられなくなった！")
+        print("もう一度選んでみよう！")
+        continue
 
 print("自分のスマホの充電が切れていることに気づいた！")
-print("しかし、A君のスマホも充電が残り2%しかない！")
-print("一回しか電話をかけられない")
-print("誰にかける？")
-print("1 消防署に電話して助けを求める")
-print("2 友達に電話して助けを求める")
-print("3 親に電話して最後のお別れをする")
+print("しかし、A君のスマホも充電が残り2%しかない...")
+print("一回しか電話をかけられないが、誰にかける？")
+print("１消防署に電話して助けを求める")
+print("２友達に電話して助けを求める")
+print("３親に電話して最後のお別れをする")
 
-selectb=int(input("1~3の中から選んでね！"))
+for selectd in range(1 and 3):
+    selectd=int(input("1~3の中から選んでね!"))
 
-if selectb==2:
-    print("友達は大富豪でヘリコプターを呼んでくれて助かった！")
+    if selectd==2:
+        print("友達は大富豪でヘリコプターを呼んでくれて助かった！")
+        print("脱出成功！！")
+        break
 
-elif selectb==1:
-    print("消火が間に合わず助からなかった")
+    elif selectd==1:
+        print("消火が間に合わず助からなかった...")
+        print("もう一度選んでみよう！")
+        continue
 
-elif selectb==3:
-    print("親に電話をかけようとした途端充電が切れた")
+    elif selectd==3:
+        print("電話をかけた途端充電がなくなってしまった...")
+        print("もう一度選んでみよう！")
+        continue
