3574ba77bf5ffbad017b9540ca3756d1ace4f7b4
moeno39 <r202202197cv@jindai.jp>
2024-08-06 03:02:40 +0900

    20240806

--- doyolab/js/mo.js
+++ doyolab/js/mo.js
@@ -24,3 +24,70 @@ document.addEventListener("DOMContentLoaded", function() {
 $(".openbtn").click(function () {
     $(this).toggleClass('active');
 });
+
+$('.slider').slick({
+    fade:true,//切り替えをフェードで行う。初期値はfalse。
+    autoplay: true,//自動的に動き出すか。初期値はfalse。
+    autoplaySpeed: 3000,//次のスライドに切り替わる待ち時間
+    speed:1000,//スライドの動きのスピード。初期値は300。
+    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+    slidesToShow: 1,//スライドを画面に3枚見せる
+    slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
+    arrows: true,//左右の矢印あり
+    prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+    nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+    dots: true,//下部ドットナビゲーションの表示
+    pauseOnFocus: false,//フォーカスで一時停止を無効
+    pauseOnHover: false,//マウスホバーで一時停止を無効
+    pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
+});
+
+//スマホ用：スライダーをタッチしても止めずにスライドをさせたい場合
+$('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
+$('.slider').slick('slickPlay');
+});
+
+//スクロールをするとハンバーガーメニューに変化するための設定を関数でまとめる
+function FixedAnime() {
+	//ヘッダーの高さを取得
+	var headerH = $('#header').outerHeight(true);
+	var scroll = $(window).scrollTop();
+	if (scroll >= headerH){//ヘッダーの高さ以上までスクロールしたら
+			$('.openbtn').addClass('fadeDown');//.openbtnにfadeDownというクラス名を付与して
+			$('#header').addClass('dnone');//#headerにdnoneというクラス名を付与
+		}else{//それ以外は
+			$('.openbtn').removeClass('fadeDown');//fadeDownというクラス名を除き
+			$('#header').removeClass('dnone');//dnoneというクラス名を除く
+		}
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+	FixedAnime();//スクロールをするとハンバーガーメニューに変化するための関数を呼ぶ
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+	FixedAnime();//スクロールをするとハンバーガーメニューに変化するための関数を呼ぶ
+});
+
+
+//ボタンをクリックした際のアニメーションの設定
+$(".openbtn").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#header").toggleClass('panelactive');//ヘッダーにpanelactiveクラスを付与
+});
+$("#g-navi li a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#header").removeClass('panelactive');//ヘッダーのpanelactiveクラスも除去
+});
+
+
+//リンク先のidまでスムーススクロール
+//※ページ内リンクを行わない場合は不必要なので削除してください
+    $('#g-navi li a').click(function () {
+	var elmHash = $(this).attr('href');
+	var pos = $(elmHash).offset().top-0;
+	$('body,html').animate({scrollTop: pos}, 1000);
+	return false;
+});
