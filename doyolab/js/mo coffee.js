01f89f71e3d9c6b6fe3cfed1165c0e1e66d87912
moeno39 <r202202197cv@jindai.jp>
2025-01-05 03:31:32 +0900

    20250105

---
+++ doyolab/js/mo coffee.js
@@ -0,0 +1,10 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    menuToggle.addEventListener('click', function() {
+        navLinks.classList.toggle('active');
+    });
+});
